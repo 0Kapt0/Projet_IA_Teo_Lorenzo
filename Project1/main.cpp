e26a5ac2f494d73d6b5db4d5c0d3720478b51f07
TeoXpok <goncalves.teo04@gmail.com>
2025-02-14 11:47:27 +0100

    oeoeoe

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -30,7 +30,7 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if (Keyboard::Escape) {
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
             window.close();
         }
         player.update(deltaTime, grid);
