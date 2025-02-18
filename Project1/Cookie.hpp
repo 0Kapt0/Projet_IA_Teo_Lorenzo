edeac2c21f4345220679f2241e38e5f358cea00a
TeoXpok <goncalves.teo04@gmail.com>
2025-02-18 17:29:33 +0100

    ajout de cookie

---
+++ Project1/Cookie.hpp
@@ -0,0 +1,14 @@
+#ifndef COOKIE_HPP
+#define COOKIE_HPP
+
+#include "Entity.hpp"
+
+class Cookie : public Entity {
+public:
+
+    Cookie(float x, float y);
+    void update(float deltaTime, Grid& grid) override;
+
+};
+
+#endif
